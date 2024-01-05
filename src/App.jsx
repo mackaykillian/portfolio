530cd8c29c6f5e11e5f96dc305de1e7292c4e735
MacKay Killian <mackay.killian@gmail.com>
2024-01-05 16:52:46 -0700

    trying out arrow blob for hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-// import { useRef } from "react";
-
-// import gsap from "gsap"; // <-- import GSAP
-// import { useGSAP } from "@gsap/react"; // <-- import the hook from our React package
-
+import React, { useRef } from "react";
+import gsap from "gsap"; // <-- import GSAP
+import { useGSAP } from "@gsap/react"; // <-- import the hook from our React package
 import { BriefcaseIcon } from "@heroicons/react/24/solid";
+import SplitType from "split-type";
 
 export default function App() {
+  const container = useRef();
+  const arrowblob = useRef();
+  useGSAP(
+    () => {
+      gsap.from(".quick-up", { y: 80, stagger: 0.1 });
+      gsap.from("#arrowblob", { y: -1000, ease: "bounce.out", duration: 1.5 });
+    },
+    { scope: container }
+  );
+
+  console.log(React.version);
+
   return (
     <div className="app">
-      <div className="relative flex flex-col h-screen page-one">
+      <div ref={container} className="relative flex flex-col h-screen page-one">
         <div className="flex flex-row justify-end flex-none pt-6 pb-6 pr-14 header">
-          <div className="text-[32px] font-display">
+          <div className="text-[32px] font-display quick-up">
             <span className="font-bold">MacKay</span>
             <span className="font-light">Killian</span>
           </div>
         </div>
         <div className="flex flex-col items-end justify-end flex-1 font-display">
-          <div className="text-[250px] text-right leading-none mb-[120px] font-medium">
-            Creative Developer
+          <div className="text-[250px] text-right leading-none font-medium quick-up">
+            Creative
+          </div>
+          <div
+            id="heading"
+            className="text-[250px] text-right leading-none mb-[120px] font-medium quick-up"
+          >
+            Developer
           </div>
         </div>
         <div>
@@ -27,6 +44,12 @@ export default function App() {
               open to work
             </div>
           </div>
+        </div>
+        <div
+          id="arrowblob"
+          className="flex flex-row items-center justify-center w-[200px] h-[200px] bg-red-500 rounded-full absolute bottom-[120px] left-[200px]"
+        >
+          <div className="">a</div>
         </div>
       </div>
     </div>
