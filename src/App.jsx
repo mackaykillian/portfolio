11cef84e30bd493a096e505dc32ccb74d7f751fa
MacKay Killian <mackay.killian@gmail.com>
2024-01-05 14:08:02 -0700

    added fonts Supreme and Poppins to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useRef } from "react";
+// import { useRef } from "react";
 
-import gsap from "gsap"; // <-- import GSAP
-import { useGSAP } from "@gsap/react"; // <-- import the hook from our React package
+// import gsap from "gsap"; // <-- import GSAP
+// import { useGSAP } from "@gsap/react"; // <-- import the hook from our React package
 
 export default function App() {
-  const container = useRef();
-
-  useGSAP(
-    () => {
-      // gsap code here...
-      gsap.to(".box", { rotation: 180 }); // <-- automatically reverted
-    },
-    { scope: container }
-  ); // <-- scope for selector text (optional)
-
   return (
-    <div ref={container} className="app">
-      <div className="box">Hello</div>
+    <div className="app">
+      <div className="flex flex-col h-screen bg-red-50 page-one">
+        <div className="flex-none header">
+          <div>MacKayKillian</div>
+        </div>
+        <div className="flex-1 bg-blue-50 font-display">Creative Developer</div>
+      </div>
     </div>
   );
 }
