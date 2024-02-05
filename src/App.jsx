d305e2dd6b372f2639e75cd5a1f4a53ff93f006f
MacKay Killian <mackay.killian@gmail.com>
2024-02-05 15:21:00 -0700

    added overview section basics

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,14 @@ export default function App() {
   );
   return (
     <div className="app">
-      <div className="mx-[16px]">
+      <div>
         <div className="flex flex-row justify-between bg-green-400">
           <div>photo</div>
           <div>nav</div>
         </div>
         <div
           ref={introFrame}
-          className="flex flex-col h-[60vh] md:h-[80vh]  justify-center bg-blue-100"
+          className="flex flex-col h-[60vh] md:h-[80vh] justify-center mx-[16px] md:mx-[32px] lg:mx-[40px]"
         >
           <div className="mt-[20vh] font-bold uppercase font-display text-[13vw] leading-[13vw] xl:text-[180px] xl:leading-[180px]">
             <div id="hero-text">
@@ -48,6 +48,53 @@ export default function App() {
                 <span className="quick-bold">p</span>
                 <span className="quick-bold">e</span>
                 <span className="quick-bold">r</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="overviewFrame" className="h-fit bg-pgray-100">
+          <div className="mx-[16px] md:mx-[32px] lg:mx-[40px]">
+            <div className="flex flex-row justify-end py-8">
+              <div className="text-5xl font-bold font-display md:text-6xl lg:text-7x">
+                What I do best
+              </div>
+            </div>
+            <div id="cards-container" className="flex flex-col ">
+              <div className="flex flex-col w-full p-12 bg-transparent border rounded-md mb-9 h-fit ">
+                <div className="text-3xl font-bold font-display">
+                  Frontend Development
+                </div>
+                <div className="mt-9">Skills</div>
+                <div className="font-sans mt-9">
+                  Lorem ipsum dolor sit amet consectetur. Sit congue ipsum netus
+                  sit vitae pellentesque. Nunc porttitor montes sollicitudin sit
+                  au1gue.
+                </div>
+              </div>
+              <div className="flex flex-col w-full p-12 bg-transparent border rounded-md mb-9 h-fit ">
+                <div className="text-3xl font-bold font-display">
+                  AR & Spatial Computing
+                </div>
+                <div className="mt-9">Skills</div>
+                <div className="font-sans mt-9">
+                  Lorem ipsum dolor sit amet consectetur. Sit congue ipsum netus
+                  sit vitae pellentesque. Nunc porttitor montes sollicitudin sit
+                  au1gue.
+                </div>
+              </div>
+              <div className="flex flex-col w-full p-12 bg-transparent border rounded-md mb-9 h-fit ">
+                <div className="text-3xl font-bold font-display">
+                  Rigorous Testing
+                </div>
+                <div className="mt-9">2+ years of experience in QA</div>
+                <div className="font-sans mt-9">
+                  Lorem ipsum dolor sit amet consectetur. Sit congue ipsum netus
+                  sit vitae pellentesque. Nunc porttitor montes sollicitudin sit
+                  au1gue.
+                </div>
+              </div>
+              <div className="flex flex-row items-end justify-end h-24 p-4 rounded-md bg-pgray-900 mb-9">
+                <div className="font-sans text-lg text-pgray-100">Resume</div>
               </div>
             </div>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,19 @@ module.exports = {
         display: ["Clash Grotesk", ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      pgray: {
+        100: "#FBFBFB",
+        200: "#EAEAEA",
+        300: "#D2D2D2",
+        500: "#A1A1A1",
+        700: "#494949",
+        900: "#151515",
+      },
+    },
   },
   plugins: [],
 };
