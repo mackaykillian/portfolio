e66b49e0144872e9e332111ba5566467fea8c4e5
MacKay Killian <mackay.killian@gmail.com>
2024-02-08 15:05:02 -0700

    added photo, heading and introduction content

--- src/views/AboutView.jsx
+++ src/views/AboutView.jsx
@@ -1,5 +1,27 @@
 function AboutView() {
-  return <div className="h-fit bg-pgray-200"></div>;
+  return (
+    <div className="h-fit bg-pgray-200">
+      <div className="">
+        <div className="flex flex-row h-[1000px]">
+          <div className="mx-4 grow md:px-8 lg:px-10">
+            <div className="mt-32 mb-16 text-5xl font-bold font-display md:text-6xl lg:text-7x">
+              About Me
+            </div>
+            <div className="max-w-[600px]">
+              Lorem ipsum dolor sit amet consectetur. Turpis leo praesent sit
+              volutpat ut vestibulum non feugiat. Elementum elit consequat
+              potenti amet non quisque scelerisque mi. Tristique quisque libero
+              lorem sit quam pellentesque. Pharetra massa placerat lacus
+              ultricies pulvinar pretium. Sit dictum mi leo.
+            </div>
+          </div>
+          <div className="bg-[url('./assets/picture-of-me.png')] w-[400px] bg-cover">
+            hello
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 AboutView.defaultProps = {};
