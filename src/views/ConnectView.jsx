85681a1fac885333ee71382828bffc0d188ecc25
MacKay Killian <mackay.killian@gmail.com>
2024-02-08 17:20:38 -0700

    created connect buttons and connect tile. Placed them in bento style ConnectView

--- src/views/ConnectView.jsx
+++ src/views/ConnectView.jsx
@@ -1,5 +1,25 @@
+import ConnectTile from "../components/ConnectTile";
+import ConnectBrandButton from "../components/ConnectBrandButton";
+
 function ConnectView() {
-  return <div className="bg-pgray-900 h-36"></div>;
+  return (
+    <div className="bg-pgray-900 h-fit">
+      <div className="flex flex-col p-6 space-y-4 sm:space-x-4 sm:space-y-0 sm:flex-row">
+        <div className="grow">
+          <ConnectTile />
+        </div>
+        <div className="flex flex-col space-y-4 sm:space-y-0 sm:justify-between">
+          <div className="flex flex-row space-x-4 h-fit">
+            <ConnectBrandButton icon="LinkedIn" />
+            <ConnectBrandButton icon="Github" />
+          </div>
+          <div className="font-sans text-sm text-pgray-300">
+            Salt Lake City, USA <br /> Custom build using React and GSAP
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ConnectView.defaultProps = {};
