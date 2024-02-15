79c6ce8f503460da1aebdf85e749eb7da13d6325
MacKay Killian <mackay.killian@gmail.com>
2024-02-15 16:49:16 -0700

    added summary section heading

--- src/App.jsx
+++ src/App.jsx
@@ -21,11 +21,11 @@ export default function App() {
 
   return (
     <div className="app">
-      <div className="flex flex-col px-6 sm:px-8 lg:flex-row lg:px-10 xl:px-20">
-        <div className="w-full bg-neutral-300 pt-4 sm:pt-6 lg:w-3/12 lg:pt-16">
+      <div className="flex flex-col bg-neutral-900 px-6 sm:px-8 lg:flex-row lg:px-10 xl:px-20">
+        <div className="w-full pb-4 pt-4 sm:pb-6 sm:pt-6 lg:w-3/12 lg:pb-0 lg:pt-16">
           {lgScreen ? <Navigation size="lg" /> : <Navigation size="sm" />}
         </div>
-        <div className="mx-auto w-full bg-neutral-600 sm:w-10/12 lg:w-9/12">
+        <div className="mx-auto w-full pt-12 sm:w-10/12 lg:w-9/12 lg:pt-16">
           <SummarySection />
         </div>
       </div>
--- src/views/SummarySection.jsx
+++ src/views/SummarySection.jsx
@@ -2,9 +2,12 @@ import Navigation from "../components/Navigation";
 
 function SummarySection() {
   return (
-    <div className="flex h-screen w-full flex-row text-neutral-200">
-      <div className="">
-        <p className="text-neutral-100">Salt Lake Based</p>
+    <div className="flex h-screen w-full flex-row">
+      <div className="font-display text-[40px] font-black text-neutral-700 sm:text-[48px] xl:text-[60px]">
+        <span className="text-neutral-100">Salt Lake City</span> based{" "}
+        <span className="text-neutral-100">developer</span> crafting modern{" "}
+        <span className="text-neutral-100">frontend</span> and{" "}
+        <span className="text-neutral-100">XR</span> solutions
       </div>
     </div>
   );
